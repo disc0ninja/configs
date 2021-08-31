f1963e46825ccd51fe54c0aec866af5eb4217e6f
disc0ninja <disc0ninja77@gmail.com>
2021-08-30 19:37:38 -0500

    refactored install.sh into a for loop and got it passing shellcheeck

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
-#! /bin/sh
+#! /bin/bash
 
 # This script will make backups of existing config files
-# in ~/.backup-dot-configs, and replace the config files
+# in "$HOME"/.backup-dots, and replace the config files
 # with the config files in the git repo "github.com/disc0ninja/configs"
 
 CLONE_LOCATION=$(pwd)
-cd
-mkdir .backup-dot-configs
-cd .backup-dot-configs
-mv ~/.bashrc ./.bashrc.back
-mv ~/.zshrc ./.zshrc.back
-mv ~/.alacritty.yml ./.alacritty.yml.back
-mv ~/.vimrc ./.vimrc.back
-ln -s $CLONE_LOCATION/.bashrc ~/.bashrc
-ln -s $CLONE_LOCATION/.zshrc ~/.zshrc 
-ln -s $CLONE_LOCATION/.alacritty.yml ~/.alacritty.yml  
-ln -s $CLONE_LOCATION/.vimrc ~/.vimrc 
+# make a directory to store backup dotfiles
+# and back up the dotfiles.
+# then make symlinks from the dotfiles here
+# to "$HOME"/.$dotfile
+mkdir "$HOME"/.backup-dots
+for dotfile in .alacritty.yml .bashrc .vimrc .zshrc 
+do
+  if [[ -e "$HOME"/$dotfile ]]
+  then
+    mv "$HOME"/$dotfile "$HOME"/.backup-dots/$dotfile.bak
+  fi
+  ln -s "$CLONE_LOCATION"/$dotfile "$HOME"/$dotfile
+done
 
-mkdir -p $HOME/.local/bin
-ln -s $CLONE_LOCATION/quick_search.sh $HOME/.local/bin/quick_search
+mkdir -p "$HOME"/.local/bin
+ln -s "$CLONE_LOCATION"/quick_search.sh "$HOME"/.local/bin/quick_search
 
 exit 0
