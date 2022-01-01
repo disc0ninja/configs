a2d0eeecef4d2ecc71f4780770ec7af80b3340a6
disc0ninja <disc0ninja77@gmail.com>
2022-01-01 10:18:01 -0600

    Moves Config class into it's own firl and cleans up some more styling pinconsitencies

--- main.py
+++ main.py
@@ -1,52 +1,6 @@
 #!/usr/bin/python3
 
-# config imports
-import sys
-import os
-
-class Config:
-
-    def __init__(self,name, src, dest):
-        self.src = src
-        self.name = name
-        self.dest = dest
-
-
-    def backup_existing(self):
-       print(F'\n\nAttempting to make a backup of {self.name}')
-       if os.path.exists(self.dest):
-           os.system(
-                   F'cp {self.dest} {self.dest}.bak'
-           )
-           print(F'Backup created at {self.dest}.bak')
-       else:
-           print(F'Nothing to backup at:\n{self.dest}')
-
-    
-    def install(self):
-       print(F'\n\nAttempting to install {self.name} to {self.dest}')
-       if os.path.exists(self.src):
-           os.system(
-                   F'cp {self.src} {self.dest}'
-           )
-           print(F'{self.name} installed to {self.dest}')
-       else:
-           print(F'Nothing to install')
-
-
-    def revert(self):
-        # restore the last backup copy if it exists
-       print(F'\n\nChecking for backup to revert to')
-       if os.path.exists(F'{self.dest}.bak'):
-           print(F'Attempting to revert {self.name} from backup')
-           os.system(
-                   F'cp {self.dest}.bak {self.dest}'
-           )
-           print(F'{self.name} reverted successfully')
-       else:
-           print(F'No backup found. Unable to revert')
-
-
+from config import Config
 
 t1 = Config('test', '/home/disc0ninja/configs/test', '/home/disc0ninja/configs/dest/test')
 t1.backup_existing()
