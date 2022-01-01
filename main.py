57570ba7081d5c88f9007d1840134261fc273b8f
disc0ninja <disc0ninja77@gmail.com>
2022-01-01 10:01:03 -0600

    Adds basic functionality to revert method

--- main.py
+++ main.py
@@ -13,34 +13,43 @@ class config:
 
 
     def backup_existing(self):
-       print(F"Attempting to make a backup of {self.name}\n")
+       print(F"\n\nAttempting to make a backup of {self.name}")
        if os.path.exists(self.dest):
            os.system(
                    F"cp {self.dest} {self.dest}.bak"
            )
-           print(F"Backup created at {self.dest}.bak\n")
+           print(F"Backup created at {self.dest}.bak")
        else:
-           print(F"Nothing to backup at:\n{self.dest}.\n\nMoving on..\n\n")
+           print(F"Nothing to backup at:\n{self.dest}")
 
     
     def install(self):
-       print(F"Attempting to install {self.name} to {self.dest}\n")
+       print(F"\n\nAttempting to install {self.name} to {self.dest}")
        if os.path.exists(self.src):
            os.system(
                    F"cp {self.src} {self.dest}"
            )
-           print(F"{self.name} installed to {self.dest}\n")
+           print(F"{self.name} installed to {self.dest}")
        else:
-           print(F"Nothing to install\n")
+           print(F"Nothing to install")
 
 
     def revert(self):
         # restore the last backup copy if it exists
-        pass
+       print(F"\n\nChecking for backup to revert to")
+       if os.path.exists(F"{self.dest}.bak"):
+           print(F"Attempting to revert {self.name} from backup")
+           os.system(
+                   F"cp {self.dest}.bak {self.dest}"
+           )
+           print(F"{self.name} reverted successfully")
+       else:
+           print(F"No backup found. Unable to revert")
 
 
 
 t1 = config('test', '/home/disc0ninja/configs/test', '/home/disc0ninja/configs/dest/test')
 t1.backup_existing()
 t1.install()
+t1.revert()
 
