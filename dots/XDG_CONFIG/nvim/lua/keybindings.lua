1f8af018fe9daa73ee4758fd92f0b6ddd732426b
disc0ninja <disc0ninja77@gmail.com>
2022-09-11 12:22:43 -0500

    adds some more nvim bindings

--- dots/XDG_CONFIG/nvim/lua/keybindings.lua
+++ dots/XDG_CONFIG/nvim/lua/keybindings.lua
@@ -3,8 +3,16 @@
 -- Leader
 vim.g.mapleader = ' '
 
+-- Reload
+vim.api.nvim_set_keymap('n', '<Leader>ll', ':source ~/.config/nvim/init.lua<CR>', { noremap = true, silent = true })
+
+-- splits
 vim.api.nvim_set_keymap('n', '<Leader>;', ':vne<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', "<Leader>'", ':new<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>j', '<C-w>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>k', '<C-W><C-J>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>l', '<C-W><C-K>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>h', '<C-W><C-H>', { noremap = true, silent = true })
 
 -- telescope
 vim.api.nvim_set_keymap('n', '<Leader>ff', [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
@@ -14,3 +22,8 @@ vim.api.nvim_set_keymap('n', '<Leader>fgg', [[<Cmd>lua require('telescope.builti
 
 -- nvim-tree
 vim.api.nvim_set_keymap('n', '<Leader>nn', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- buffers
+vim.api.nvim_set_keymap('n', '<Leader>bn', ':bn<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>bb', ':bp<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>bd', ':bd%<CR>', { noremap = true, silent = true })
