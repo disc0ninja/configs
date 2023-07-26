a19275698c53c33015d949e803bd6e2319fd3423
disc0ninja <disc0ninja77@gmail.com>
2023-07-25 19:28:47 -0500

    Adds tmux config back and sets some custom keys for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -22,8 +22,8 @@ return {
   --color_scheme = "Abernathy",
   color_scheme = "Abernathy",
   inactive_pane_hsb = {
-    saturation = 0.7,
-    brightness = 0.7,
+    saturation = 1.0,
+    brightness = 1.2,
   },
   font_size = font_size,
   use_fancy_tab_bar = false,
@@ -51,46 +51,25 @@ return {
   --------------------------------------------------
   -- Key Maps
   --------------------------------------------------
-  --   leader = { key = 'Alt', timeout_milliseconds = 1500 },
-  --   keys = {
-  --     --------------------------------------------------
-  --     -- Window/Pane Management
-  --     --------------------------------------------------
-  --     { key = ';', mods = 'ALT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  --     { key = "'", mods = 'ALT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-  --     { key = 'h', mods = 'ALT', action = act.ActivatePaneDirection 'Left' },
-  --     { key = 'j', mods = 'ALT', action = act.ActivatePaneDirection 'Down' },
-  --     { key = 'k', mods = 'ALT', action = act.ActivatePaneDirection 'Up' },
-  --     { key = 'l', mods = 'ALT', action = act.ActivatePaneDirection 'Right' },
-  --     { key = 'h', mods = 'LEADER', action = act.AdjustPaneSize { 'Left', 5 } },
-  --     { key = 'j', mods = 'LEADER', action = act.AdjustPaneSize { 'Down', 5 } },
-  --     { key = 'k', mods = 'LEADER', action = act.AdjustPaneSize { 'Up', 5 } },
-  --     { key = 'l', mods = 'LEADER', action = act.AdjustPaneSize { 'Right', 5 } },
-  --     { key = 'r', mods = 'LEADER', action = act.ActivateKeyTable { name = 'resize_panes', timeout_milliseconds = 15000, replace_current = false, one_shot = false } },
-  --     { key = 'R', mods = 'ALT', action = act.ResetFontAndWindowSize },
-  --     { key = 'Space', mods = 'LEADER', action = act.RotatePanes 'Clockwise' },
-  --     { key = 'Space', mods = 'LEADER|SHIFT', action = act.RotatePanes 'CounterClockwise', },
-  --     { key = 's', mods = 'LEADER', action = act.PaneSelect { alphabet = '1234567890', }, },
-  --     { key = 'S', mods = 'LEADER', action = act.PaneSelect { mode = 'SwapWithActive', } },
-  --     --{ key="z" , mods="LEADER", action = "TogglePaneZoomState" },
-  --     { key="z" , mods="LEADER", action = act.ActivateKeyTable { name = 'zoom_actions', timeout_milliseconds = 1000, replace_current = false, one_shot = false} },
-  --     { key="x", mods="LEADER", action  = act { CloseCurrentPane = { confirm = true } } },
-  --   },
-  --   --------------------------------------------------
-  --   -- Key Tables
-  --   --------------------------------------------------
-  --   key_tables = {
-  --     resize_panes = {
-  --       { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
-  --       { key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
-  --       { key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
-  --       { key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
-  --       { key = 'Escape', action = act.PopKeyTable }
-  --     },
-  --     zoom_actions = {
-  --       { key="z" , action = "TogglePaneZoomState" },
-  --       { key="+" , action = act.IncreaseFontSize },
-  --       { key="-" , action = act.DecreaseFontSize },
-  --     },
-  --   },
+  leader = { key = 'a', mods = 'ALT', timeout_milliseconds = 1500 },
+  keys = {
+    --------------------------------------------------
+    -- Window/Pane Management
+    --------------------------------------------------
+    { key = ';',     mods = 'ALT',          action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = "'",     mods = 'ALT',          action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'h',     mods = 'ALT',          action = act.ActivatePaneDirection 'Left' },
+    { key = 'j',     mods = 'ALT',          action = act.ActivatePaneDirection 'Down' },
+    { key = 'k',     mods = 'ALT',          action = act.ActivatePaneDirection 'Up' },
+    { key = 'l',     mods = 'ALT',          action = act.ActivatePaneDirection 'Right' },
+    { key = 'h',     mods = 'ALT|SHIFT',    action = act.AdjustPaneSize { 'Left', 5 } },
+    { key = 'j',     mods = 'ALT|SHIFT',    action = act.AdjustPaneSize { 'Down', 5 } },
+    { key = 'k',     mods = 'ALT|SHIFT',    action = act.AdjustPaneSize { 'Up', 5 } },
+    { key = 'l',     mods = 'ALT|SHIFT',    action = act.AdjustPaneSize { 'Right', 5 } },
+    { key = 'Space', mods = 'LEADER',       action = act.RotatePanes 'Clockwise' },
+    { key = 'Space', mods = 'LEADER|SHIFT', action = act.RotatePanes 'CounterClockwise', },
+    { key = 's',     mods = 'LEADER',       action = act.PaneSelect { alphabet = '1234567890', }, },
+    { key = "z",     mods = "LEADER",       action = "TogglePaneZoomState" },
+  },
+  --------------------------------------------------
 }
