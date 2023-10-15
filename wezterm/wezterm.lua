d0fec7edcb4c8e0b4fd146afd87f9e95bc5361fb
disc0ninja <disc0ninja77@gmail.com>
2023-10-15 16:18:54 -0500

    Adds Navigator config to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,6 +12,39 @@ if wezterm.target_triple == 'x86_64-apple-darwin' then
   end)
 end
 
+-- Navigator.nvim
+local function isViProcess(pane)
+  -- get_foreground_process_name On Linux, macOS and Windows,
+  -- the process can be queried to determine this path. Other operating systems
+  -- (notably, FreeBSD and other unix systems) are not currently supported
+  return pane:get_foreground_process_name():find('n?vim') ~= nil
+  -- return pane:get_title():find("n?vim") ~= nil
+end
+
+local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
+  if isViProcess(pane) then
+    window:perform_action(
+    -- This should match the keybinds you set in Neovim.
+      act.SendKey({ key = vim_direction, mods = 'ALT' }),
+      pane
+    )
+  else
+    window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+  end
+end
+
+wezterm.on('ActivatePaneDirection-right', function(window, pane)
+  conditionalActivatePane(window, pane, 'Right', 'l')
+end)
+wezterm.on('ActivatePaneDirection-left', function(window, pane)
+  conditionalActivatePane(window, pane, 'Left', 'h')
+end)
+wezterm.on('ActivatePaneDirection-up', function(window, pane)
+  conditionalActivatePane(window, pane, 'Up', 'k')
+end)
+wezterm.on('ActivatePaneDirection-down', function(window, pane)
+  conditionalActivatePane(window, pane, 'Down', 'j')
+end)
 
 return {
   send_composed_key_when_left_alt_is_pressed = false,
@@ -51,17 +84,21 @@ return {
   --------------------------------------------------
   -- Key Maps
   --------------------------------------------------
-  leader = { key = 'a', mods = 'ALT', timeout_milliseconds = 1500 },
+  --  leader = { key = 'a', mods = 'ALT', timeout_milliseconds = 1500 },
   keys = {
     --------------------------------------------------
     -- Window/Pane Management
     --------------------------------------------------
+    { key = 'h',     mods = 'ALT',          action = act.EmitEvent 'ActivatePaneDirection-left' },
+    { key = 'j',     mods = 'ALT',          action = act.EmitEvent 'ActivatePaneDirection-down' },
+    { key = 'k',     mods = 'ALT',          action = act.EmitEvent 'ActivatePaneDirection-up' },
+    { key = 'l',     mods = 'ALT',          action = act.EmitEvent 'ActivatePaneDirection-right' },
     { key = ';',     mods = 'ALT',          action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
     { key = "'",     mods = 'ALT',          action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-    { key = 'h',     mods = 'ALT',          action = act.ActivatePaneDirection 'Left' },
-    { key = 'j',     mods = 'ALT',          action = act.ActivatePaneDirection 'Down' },
-    { key = 'k',     mods = 'ALT',          action = act.ActivatePaneDirection 'Up' },
-    { key = 'l',     mods = 'ALT',          action = act.ActivatePaneDirection 'Right' },
+    -- { key = 'h',     mods = 'ALT',          action = act.ActivatePaneDirection 'Left' },
+    -- { key = 'j',     mods = 'ALT',          action = act.ActivatePaneDirection 'Down' },
+    -- { key = 'k',     mods = 'ALT',          action = act.ActivatePaneDirection 'Up' },
+    -- { key = 'l',     mods = 'ALT',          action = act.ActivatePaneDirection 'Right' },
     { key = 'h',     mods = 'ALT|SHIFT',    action = act.AdjustPaneSize { 'Left', 5 } },
     { key = 'j',     mods = 'ALT|SHIFT',    action = act.AdjustPaneSize { 'Down', 5 } },
     { key = 'k',     mods = 'ALT|SHIFT',    action = act.AdjustPaneSize { 'Up', 5 } },
